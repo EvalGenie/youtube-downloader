665835bd9a244bf79c097b3364d7db05418d99c4
EvalGenie <130593073+EvalGenie@users.noreply.github.com>
2023-10-26 07:44:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pytube import YouTube
+import os
 
-def download_video_and_audio(url, save_path='C:\\Users\\dmiller\\Downloads'):
+def download_video_and_audio(url, save_path='/home/meeseeks/Videos'):
     # Create a YouTube object with the provided URL
     yt = YouTube(url)
 
@@ -22,8 +23,6 @@ def download_video_and_audio(url, save_path='C:\\Users\\dmiller\\Downloads'):
     print(f"Downloaded audio: {yt.title} successfully!")
 
 if __name__ == "__main__":
-    import os
-
     video_url = input("Enter the URL of the YouTube video you want to download: ")
     download_path = input(f"Enter the path where you want to save the video and audio (or press enter for default path {download_video_and_audio.__defaults__[0]}): ")
     download_video_and_audio(video_url, download_path if download_path else download_video_and_audio.__defaults__[0])
